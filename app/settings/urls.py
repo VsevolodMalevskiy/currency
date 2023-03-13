f5b2b71629d07a59977867e9f48baac4da0d29ec
Vsevolod-PC <VsevolodMalevskiy@gmail.com>
2023-03-13 16:58:13 +0200

    HW12

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import views
+# from django.contrib.auth import views
 from django.urls import path, include
 from currency.views import IndexView, ProfileView, RegisterUser
 
@@ -10,14 +10,14 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('register/', RegisterUser.as_view(), name='register'),
-
-    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
-    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    #
+    # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #
+    # path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('__debug__/', include('debug_toolbar.urls')),
 
