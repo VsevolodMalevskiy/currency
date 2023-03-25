12415808636f29cf16cf20f82066d732aebc34ea
VsevolodMalevskiy <VsevolodMalevskiy@gmail.com>
2023-03-25 15:33:21 +0200

    111

--- app/currency/views.py
+++ app/currency/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
-#
 
 
 class RateListView(ListView):
